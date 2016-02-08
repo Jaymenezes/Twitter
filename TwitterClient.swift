caf131e94c8bad93b96a760aaae799dc1deba6b5
JeanMenezes <jottamenezes@gmail.com>
2016-02-08 17:29:13 -0600

    Static Instance and Action Button
    Created Static Instance and Basic authorization through action button

--- TwitterClient.swift
+++ TwitterClient.swift
@@ -9,7 +9,24 @@
 import UIKit
 import BDBOAuth1Manager
 
+
+let twitterConsumerKey = "cYH6p8Uo2U6Sfjj2B6WThBFvD"
+let twitterConsumerSecret = "UIwv3egePK7V75LI5w1an01OIL7EHrMgrYxzD5TReisBfo7JJ4"
+let twitterBaseURL = NSURL(string: "https://api.twitter.com")
+
+
+
 class TwitterClient: BDBOAuth1SessionManager {
+    
+    class var sharedInstance: TwitterClient {
+        struct Static {
+            static let instance = TwitterClient(baseURL: twitterBaseURL, consumerKey: twitterConsumerKey, consumerSecret: twitterConsumerSecret)
+        }
+        return Static.instance
+        
+    }
+    
+    
     
 
 }
