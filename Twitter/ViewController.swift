7c19913ab9ad850e74f3adb35bad89d3dd49185b
JeanMenezes <jottamenezes@gmail.com>
2016-02-08 16:10:10 -0600

    Follow-up video 1
    Finished installing cocoa pods, started twitter client class

---
+++ Twitter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Twitter
+//
+//  Created by user on 2/8/16.
+//  Copyright © 2016 Jean. All rights reserved.
+//
+
+import UIKit
+import AFNetworking
+import BDBOAuth1Manager
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TwitterClient.swift
@@ -0,0 +1,15 @@
+//
+//  TwitterClient.swift
+//  Twitter
+//
+//  Created by user on 2/8/16.
+//  Copyright © 2016 Jean. All rights reserved.
+//
+
+import UIKit
+import BDBOAuth1Manager
+
+class TwitterClient: BDBOAuth1SessionManager {
+    
+
+}
